e67a8b6b0aea8e07967e762d0194d645d87ece2c
llcain <llcain2012@gmail.com>
2023-04-08 20:04:52 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import Car from './components/Car';
+
+// const element = <h1>Hello World</h1>;
+
+// const colors =["red", "green", "blue"];
+// const items = colors.map(color => `<li>${color}</li>`);
+// console.log(items);
+
+// const myelement = (
+//     <table>
+//       <tr>
+//         <th>Name</th>
+//       </tr>
+//       <tr>
+//         <td>John</td>
+//       </tr>
+//       <tr>
+//         <td>Elsa</td>
+//       </tr>
+//     </table>
+//   );
+
+// const myelement = <h1>LaQuisha Cain</h1>
+
+//  ReactDom.render(myelement, document.getElementById('root'));
+
+
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(<Car />);
+ 
+
+
+
